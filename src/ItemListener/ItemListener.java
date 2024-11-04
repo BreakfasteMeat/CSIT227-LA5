35821a27dbeb42737f2bda6d8a558b5bd5a1d7e0
LAB14_USER ON WS09 <L14X07W09@citulab.net>
2024-11-04 10:28:47 +0800

    ItemListener add

---
+++ src/ItemListener/ItemListener.java
@@ -0,0 +1,12 @@
+package ItemListener;
+
+import javax.swing.*;
+
+public class ItemListener extends JFrame {
+
+    private JPanel panel1;
+
+    public JPanel getPanel1() {
+        return panel1;
+    }
+}
---
+++ src/ItemListener/Main.java
@@ -0,0 +1,13 @@
+package ItemListener;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        ItemListener app = new ItemListener();
+        app.setContentPane(app.getPanel1());
+        app.setSize(300,300);
+        app.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        app.setVisible(true);
+    }
+}
